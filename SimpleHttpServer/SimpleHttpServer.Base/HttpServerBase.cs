c9a62a21dd5932238ba9e95204a801351cf4aefe
Milo Wical <Milo.Wical@compare.com>
2018-07-25 17:06:33 -0400

    Added a Dockerfile to get the server to listen on port 80 from a container.

--- SimpleHttpServer/SimpleHttpServer.Base/HttpServerBase.cs
+++ SimpleHttpServer/SimpleHttpServer.Base/HttpServerBase.cs
@@ -13,33 +13,50 @@
 
     public abstract class HttpServerBase
     {
+        private const int Port = 80;
+
         public void Serve()
         {
             var server = new HttpListener();
-            server.Prefixes.Add("http://" + Environment.MachineName + "/");
-            server.Prefixes.Add("http://localhost/");
+            server.Prefixes.Add($"http://*:{Port}/");
+            //server.Prefixes.Add($"http://{Environment.MachineName}:{Port}/");
+            //server.Prefixes.Add($"http://localhost:{Port}/");
+            //AddLocalIpAddresses(server.Prefixes);
 
-            AddLocalIpAddresses(server.Prefixes);
+            Console.WriteLine("Prefixes:");
+
+            foreach (var prefix in server.Prefixes)
+            {
+                Console.WriteLine(prefix);
+            }
 
             while (true)
             {
-                Console.WriteLine("Server Listening...\n");
+                Console.WriteLine("\nServer Listening...\n");
 
                 server.Start();
+                try
+                {
+                    var context = server.GetContext();
 
-                var context = server.GetContext();
+                    var responseString = ProcessRequest(context.Request);
 
-                var responseString = ProcessRequest(context.Request);
+                    Console.WriteLine("\n\nSending Response:\n-----------------\n" + responseString + "\n");
 
-                Console.WriteLine("\n\nSending Response:\n-----------------\n" + responseString + "\n");
+                    var response = context.Response;
 
-                var response = context.Response;
-
-                var buffer = Encoding.UTF8.GetBytes(responseString);
-                response.ContentLength64 = buffer.Length;
-                var output = response.OutputStream;
-                output.Write(buffer, 0, buffer.Length);
-                output.Close();
+                    var buffer = Encoding.UTF8.GetBytes(responseString);
+                    response.ContentLength64 = buffer.Length;
+                    var output = response.OutputStream;
+                    output.Write(buffer, 0, buffer.Length);
+                    output.Close();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    throw;
+                }
+                
             }
         }
 
@@ -48,7 +65,7 @@
             var host = Dns.GetHostEntry(Dns.GetHostName());
 
             foreach (var ip in host.AddressList.Where(ip => ip.AddressFamily == AddressFamily.InterNetwork))
-                prefixes.Add("http://" + ip + "/");
+                prefixes.Add($"http://{ip}:{Port}/");
         }
 
         protected abstract string ProcessRequest(HttpListenerRequest httpListenerRequest);
